a7853e7c2651464e8048678a39cd189e099db94c
David-PIC <58555256+David-PIC@users.noreply.github.com>
2023-05-01 21:34:25 +0200

    redesign

--- main.py
+++ main.py
@@ -1,75 +1,22 @@
-from flask import Flask, render_template
-
+import os
+from flask import Flask, render_template, url_for
+from jinja2 import FileSystemLoader
+import json
 app = Flask(__name__, static_folder='static')
-
 
 @app.route('/')
 def index():
     return render_template('index.html')
 @app.route('/grundlegende-befehle')
 def grundlegende_befehle():
-    tasks = [
-    {
-        'number': 1,
-        'title': 'Verzeichnis wechseln',
-        'tutorial': '../tutorial/cd.html',
-        'question': 'Wie wechseln Sie in das Verzeichnis /home/user?',
-        'answers': ['cd /home/user', 'cd /home/user/'],
-        'input_id': 'gb1',
-        'button_id': 'gb1-button',
-        'result_id': 'gb1-result'
-    },
-    {
-        'number': 2,
-        'title': 'Home-Verzeichnis wechseln',
-        'tutorial': '',
-        'question': 'Wie wechseln Sie in das Home-Verzeichnis des aktuellen Benutzers?',
-        'answers': ['cd ~', 'cd'],
-        'input_id': 'gb2',
-        'button_id': 'gb2-button',
-        'result_id': 'gb2-result'
-    },
-    {
-        'number': 3,
-        'title': 'Dateien im aktuellen Verzeichnis anzeigen',
-        'tutorial': '../tutorial/ls.html',
-        'question': 'Wie zeigen Sie die Dateien im Aktuellen Verzeichnis an? (bitte nur den Befehl ohne zusätzliche optionen)',
-        'answers': ['ls', 'ls .'],
-        'input_id': 'gb3',
-        'button_id': 'gb3-button',
-        'result_id': 'gb3-result'
-    },
-    {
-        'number': 4,
-        'title': 'Dateien im Verzeichnis /usr/local/bin anzeigen',
-        'tutorial': '',
-        'question': 'Wie zeigen Sie die Dateien im Verzeichnis /usr/local/bin an? (bitte nur den Befehl ohne zusätzliche optionen)',
-        'answers': ['ls /usr/local/bin', 'ls /usr/local/bin/'],
-        'input_id': 'gb4',
-        'button_id': 'gb4-button',
-        'result_id': 'gb4-result'
-    },
-    {
-        'number': 5,
-        'title': 'Alle Dateien inklusive versteckter Dateien im aktuellen Verzeichnis anzeigen',
-        'tutorial': '',
-        'question': 'Wie zeigen Sie die Dateien inklusive aller versteckten Dateien im aktuellen Verzeichnis an?',
-        'answers': ['ls -a', 'ls . -a'],
-        'input_id': 'gb5',
-        'button_id': 'gb5-button',
-        'result_id': 'gb5-result'
-    },
-    {
-        'number': 6,
-        'title': 'Aktuelles Verzeichnis anzeigen',
-        'tutorial': '../tutorial/pwd.html',
-        'question': 'Wie zeigen Sie das aktuelle Verzeichnis an, in dem Sie sich befinden?',
-        'answers': ['pwd'],
-        'input_id': 'gb6',
-        'button_id': 'gb6-button',
-        'result_id': 'gb6-result'
-    }
+    template_dirs = [
+    os.path.join(app.root_path, 'templates'),
+    os.path.join(app.root_path, 'sites/grundlegende_befehle')
     ]
+    app.jinja_loader = FileSystemLoader(template_dirs)
+    static_folder = os.path.join(app.root_path, 'static')
+    with open('sites/grundlegende_befehle/grundlegende-befehle.json') as f:
+       tasks = json.load(f)
     return render_template('grundlegende-befehle.html', tasks=tasks)
 
 if __name__ == '__main__':
--- static/main.js
+++ static/main.js
@@ -1,12 +1,68 @@
-function checkAnswer(task, inputId) {
-    const inputElement = document.getElementById(inputId);
-    const answer = inputElement.value.trim();
-    const resultElement = document.getElementById(`${inputId}-result`);
+function checkAnswer(taskId, correctAnswers) {
+    const input = document.getElementById(taskId);
+    const result = document.getElementById(`${taskId}-result`);
+    const button = document.getElementById(`${taskId}-button`);
 
-
-    if (correctAnswers[task].includes(answer)) {
-        resultElement.innerHTML = "Richtig! Andere mögliche Lösungen: " + correctAnswers[task].join(", ");
+    if (correctAnswers.includes(input.value.trim())) {
+        result.innerHTML = "Richtige Lösung!";
+        result.style.color = "green";
+        saveSolution(taskId, input.value.trim());
+        input.disabled = true;
+        button.disabled = true;
     } else {
-        resultElement.innerHTML = "Falsch. Versuchen Sie es noch einmal.";
+        result.innerHTML = "Falsche Lösung. Versuchen Sie es noch einmal.";
+        result.style.color = "red";
     }
 }
+
+function saveSolution(taskId, correctAnswer) {
+    localStorage.setItem(taskId, correctAnswer);
+}
+
+function loadSolutions() {
+    const tasks = document.getElementsByClassName("task");
+    const taskCount = tasks.length;
+
+    for (let i = 0; i < taskCount; i++) {
+        const input = tasks[i].querySelector(".input-field");
+        const taskId = input.getAttribute("id");
+        const prefix = taskId.substring(0, 2);
+        const solution = localStorage.getItem(taskId);
+        if (solution) {
+            const input = document.getElementById(taskId);
+            const button = document.getElementById(`${taskId}-button`);
+            input.value = solution;
+            input.disabled = true;
+            button.disabled = true;
+            const result = document.getElementById(`${taskId}-result`);
+            result.innerHTML = "Richtige Lösung!";
+            result.style.color = "green";
+          }
+        }
+      }
+
+function resetSolutionsWithDataPrefix() {
+    const button = document.querySelector('button[data-prefix]');
+    const prefix = button.getAttribute('data-prefix');
+
+    for (let i = localStorage.length - 1; i >= 0; i--) {
+        const key = localStorage.key(i);
+        if (key.startsWith(prefix)) {
+            localStorage.removeItem(key);
+        }
+    }
+    location.reload();
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    const button = document.querySelector(".button");
+    const prefix = button.dataset.prefix;
+    loadSolutions(prefix);
+});
+
+window.addEventListener("load", () => {
+    const button = document.querySelector(".button");
+    const prefix = button.dataset.prefix;
+    loadSolutions(prefix);
+});
+
